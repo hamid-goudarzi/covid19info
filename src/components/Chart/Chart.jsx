18f4187698477e4d8a3d49845c0abb7ed32ef79e
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2024-03-01 00:34:09 -0800

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,6 @@ import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
 import styles from "./Chart.module.css";
 
-
 const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const [dailyData, setDailyData] = useState([]);
 
