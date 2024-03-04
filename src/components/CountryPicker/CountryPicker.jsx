a1f3597cf8acd3927ed1359e55937af037f25e41
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2024-03-04 01:11:12 -0800

    fix

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,7 +3,6 @@ import { NativeSelect, FormControl } from "@material-ui/core";
 import styles from "./CountryPicker.module.css";
 import { fetchCountries } from "../../api";
 
-
 const CountryPicker = ({ handleCountryChange }) => {
   const [fetchedCountries, setFetchedCountries] = useState([]);
 
@@ -22,7 +21,6 @@ const CountryPicker = ({ handleCountryChange }) => {
         onChange={(e) => handleCountryChange(e.target.value)}
         className={styles.formControl}
       >
-        <option value="">Global</option>
         <option value="">Global</option>
         {fetchedCountries.map((country, i) => (
           <option key={i} value={country}>
